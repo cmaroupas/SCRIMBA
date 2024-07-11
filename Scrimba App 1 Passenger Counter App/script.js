48fc3cc30cc39d8019a5fc7c60b73cde8d4754b0
cmaroupas <cynthia.maroupas@mail.mcgill.ca>
2024-07-11 01:02:54 -0400

    Scrimba Javascript

---
+++ Scrimba App 1 Passenger Counter App/script.js
@@ -0,0 +1,20 @@
+// Get elements from the HTML
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+// Initialize count to 0
+let count = 0
+
+// Function to increase the count
+function increment() {
+    count += 1 // Add 1 to count
+    countEl.textContent = count // Update the count display
+}
+
+// Function to save the count and reset
+function save() {
+    let countStr = count + " - " // Create a string with the count
+    saveEl.textContent += countStr // Add the string to saved counts
+    countEl.textContent = 0 // Reset the count display to 0
+    count = 0 // Reset the count to 0
+}
